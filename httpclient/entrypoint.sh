2418f7de550c54380b8f9ac48d73d0475c0e940d
alice <alice@example.com>
2021-03-12 05:57:41 +0000

    discard curl output

--- httpclient/entrypoint.sh
+++ httpclient/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 while true
 do
-	curl -x squid4:3128 http://nginx/data
+	curl -o /dev/null -x squid4:3128 http://nginx/data
 	sleep 30
-	curl -x squid5:3128 http://nginx/data
+	curl -o /dev/null -x squid5:3128 http://nginx/data
 	sleep 30
 done
 
