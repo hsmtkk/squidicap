4ed2d07392f98292e1554121a91d62df952bf087
alice <alice@example.com>
2021-04-05 00:02:59 +0000

    patched squid works fine

--- httpclient/entrypoint.sh
+++ httpclient/entrypoint.sh
@@ -9,8 +9,8 @@ do
 	curl -o /dev/null -x squid5:3128 http://nginx/data
 	sleep 20
 
-	echo squid5rawip
-	curl -o /dev/null -x squid5rawip:3128 http://nginx/data
+	echo squid5patched
+	curl -o /dev/null -x squid5patched:3128 http://nginx/data
 	sleep 20
 done
 
